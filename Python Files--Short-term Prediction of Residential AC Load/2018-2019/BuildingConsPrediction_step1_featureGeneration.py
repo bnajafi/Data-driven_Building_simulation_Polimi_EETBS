3af2d014753136ff255e0e90b91c45713dd0201e
bnajafi <b.najafi87@gmail.com>
2018-11-29 02:50:40 +0100

    updating

--- Python Files--Short-term Prediction of Residential AC Load/2018-2019/BuildingConsPrediction_step1_featureGeneration.py
+++ Python Files--Short-term Prediction of Residential AC Load/2018-2019/BuildingConsPrediction_step1_featureGeneration.py
@@ -73,10 +73,89 @@ DF_joined.head(24)
 DF_joined_cleaned = DF_joined.dropna()
 DF_joined_cleaned.head(24)
 
-temp_min = DF_joined_cleaned["temperature"].min()
-temp_max = DF_joined_cleaned["temperature"].max()
-DF_joined_cleaned["temperature_normalized"]=(DF_joined_cleaned["temperature"]-temp_min)/(temp_max-temp_min)
-DF_joined_cleaned.head(24)
+DF_joined_cleaned_copy = DF_joined.dropna().copy()
+
+DF_joined_cleaned_chosenDates = DF_joined_cleaned_copy["2014-08-01":"2014-08-04"]
+
+# We need to solve the problem with the timezone
+DF_joined_cleaned_chosenDates["temperature"]=DF_joined_cleaned_chosenDates["temperature"].shift(-5)
+DF_joined_cleaned_chosenDates.dropna()
+DF_joined_cleaned_chosenDates.head()
+DF_joined_cleaned_chosenDates.describe()
+
+DF_joined_cleaned_chosenDates_min=DF_joined_cleaned_chosenDates.min()
+DF_joined_cleaned_chosenDates_max=DF_joined_cleaned_chosenDates.max()
+DF_joined_cleaned_chosenDates_normalized= (DF_joined_cleaned_chosenDates-DF_joined_cleaned_chosenDates_min)/(DF_joined_cleaned_chosenDates_max-DF_joined_cleaned_chosenDates_min)
+plt.figure()
+DF_joined_cleaned_chosenDates_normalized.plot()
+
+
+# Creating lagged features
+# First let me reconstruct my dataframe by applying time-zone to the whole dataset
+
+DF_joined = DF_consumption.join([DF_Temperature,DF_irradiance])
+DF_mod = DF_joined.copy()
+DF_mod["temperature"]=DF_mod["temperature"].shift(-5)
+DF_mod.dropna(inplace=True) # It is the same as writing it like this: DF_mod=DF_mod.dropna()
+# In order to consider the time-lags we will need to include 
+# all of these columns with lags (shifts)
+DF_mod.head()
+DF_mod.describe()
+
+DF_mod["Temperature -1h"]= DF_mod["temperature"].shift(1)
+DF_mod["Temperature -2h"]= DF_mod["temperature"].shift(2)
+DF_mod["Temperature -3h"]= DF_mod["temperature"].shift(2)
+DF_mod["Temperature -4h"]= DF_mod["temperature"].shift(2)
+DF_mod["Temperature -5h"]= DF_mod["temperature"].shift(2)
+DF_mod["Temperature -6h"]= DF_mod["temperature"].shift(2)
+DF_mod.head()
+
+# but there is abetter way of doing this !!
+
+ lag_start=1
+ lag_end = 6
+ lag_interval=1
+ 
+
+column_name="temperature"
+df=DF_mod
+for i in range(lag_start,lag_end+1,lag_interval):
+    new_column_name = column_name+" -"+str(i)+"hr"
+    print new_column_name
+    df[new_column_name]=df[column_name].shift(i)   
+    df.dropna(inplace=True) #this removes all the row with a Nan
+
+def lag_feature(df,column_name,lag_start,lag_end,lag_interval):
+    for i in range(lag_start,lag_end+1,lag_interval):
+        new_column_name = column_name+" -"+str(i)+"hr"
+        print new_column_name
+        df[new_column_name]=df[column_name].shift(i)   
+        df.dropna(inplace=True) #this removes all the row with a Nan
+    return df
+
+
+#DF_mod.columns =["AC_consumption","temperature","irradiance"]
+# The second way of doing this:
+DF_mod=DF_mod.rename(columns={"air conditioner_5545":"AC_consumption","gen":"irradiance"})
+
+DF_mod=  lag_feature(DF_mod,"temperature",1,6,1)
+DF_mod.head()
+# Let's lag the irraidance , I would do it just for 3 to 6 hours
+DF_mod=  lag_feature(DF_mod,"irradiance",3,6,1)
+
+# Let's add the previous consumptions in the last 24 hours!
+DF_mod=  lag_feature(DF_mod,"AC_consumption",1,24,1)
+DF_mod.head()
+DF_mod.describe()
+
+
+# Let's do the same for the irradiance and consumption
+# but I don't like the names of irradiance and consumption columns
+
+# For renaming the column names , you have two ways
+
+
+
 
 
 
